37db1369bc08f7284e7a001709629fa06a62b532
Kamindu <kaminduN@users.noreply.github.com>
2022-08-10 17:21:27 +0530

    Refactor the service port customize logic
    configurable from env variable

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,27 +1,22 @@
 package com.example.demo;
 
-import java.util.Collections;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.utils.EnvConfigReader;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(DemoApplication.class);
-        app.setDefaultProperties(Collections
-          .singletonMap("server.port", EnvConfigReader.getConfig("service.port", "8080")));
         app.run(args);
-	}
 
-	@RequestMapping("/")
-	public String home() {
-		return "Hello Docker World";
-	}
+		logger.info("Application started.");
 
+	}
 }
